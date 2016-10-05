b68e1dfade30383f3a372429c4c3eec507647cc4
IvanSlushko <lu813@tut.by>
2016-10-06 00:13:50 +0300

    06-10

--- src/main/java/training1/Reader.java
+++ src/main/java/training1/Reader.java
@@ -1,18 +1,35 @@
 package training1;
 
-import java.util.*;
+//import java.util.*;
+import javax.swing.JOptionPane;
 
 public class Reader {
-	int i;
-	int k;
-	public void Scan(){
-		System.out.println("input 1: ");
-		Scanner scn = new Scanner (System.in);
-		i = scn.nextInt();
-		System.out.println("input 2: ");
-		k = scn.nextInt();
+	int i,k;
+	String n1,n2;
+		
+	//int k;
+
+	public void Scan() {
+		
+		n1= JOptionPane.showInputDialog("Введите первое число");
+		n2= JOptionPane.showInputDialog("Введите второе число");
+		i = Integer.parseInt(n1);
+		k = Integer.parseInt(n2);
+			
+		//System.out.println("input 1: ");
+		//Scanner scn = new Scanner(System.in);
+		//i = scn.nextInt();
+		//System.out.println("input 2: ");
+		//k = scn.nextInt();
+	}
+
+	public void Scan(int a, int b) {
+		
+		JOptionPane.showMessageDialog(null, "Теперь ваше первое число = " +a);
+		JOptionPane.showMessageDialog(null, "Теперь ваше второе число = " +b);			
 		
 	}
-	// morning
+	
+	
 	
 }
--- src/main/java/training1/start.java
+++ src/main/java/training1/start.java
@@ -7,9 +7,14 @@ public class start {
 		r.Scan();
 		r.i=count (r.i);
 		r.k=count (r.k);		
-		System.out.println ("new 1 =" + r.i);
-		System.out.println ("new 2 =" + r.k); 		
+		//System.out.println ("new 1 =" + r.i);
+		//System.out.println ("new 2 =" + r.k); 		
+		r.Scan (r.i,r.k);
+	
+	
+	
 	}
+	
 	//counter plus one
 	private static int count (int x){
 		x = x + 1;
