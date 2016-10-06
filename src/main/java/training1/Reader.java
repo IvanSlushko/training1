5aeca6909d1a0a1ac4ea439a808f53d889f7c3e3
IvanSlushko <lu813@tut.by>
2016-10-07 00:39:52 +0300

    07-10 gui and JFrame proba

--- src/main/java/training1/Reader.java
+++ src/main/java/training1/Reader.java
@@ -1,35 +1,40 @@
 package training1;
 
-//import java.util.*;
-import javax.swing.JOptionPane;
+import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+//gui and JFrame
 
-public class Reader {
-	int i,k;
-	String n1,n2;
-		
-	//int k;
+public class Reader extends JFrame {
 
-	public void Scan() {
+		JButton b1,b2;
+		JLabel l1,l2,l3,l4;
+		JTextField t1,t2;
+		int i,k;
+		String a,b;
 		
-		n1= JOptionPane.showInputDialog("Введите первое число");
-		n2= JOptionPane.showInputDialog("Введите второе число");
-		i = Integer.parseInt(n1);
-		k = Integer.parseInt(n2);
-			
-		//System.out.println("input 1: ");
-		//Scanner scn = new Scanner(System.in);
-		//i = scn.nextInt();
-		//System.out.println("input 2: ");
-		//k = scn.nextInt();
+	public Reader (String s){
+		super(s);
+		setLayout(new FlowLayout ());
+		b1=new JButton ("Очистить");
+		b2=new JButton ("Посчитать");
+		l1=new JLabel ("Введите первое число:");
+		l2=new JLabel ("Введите второе число:");
+		l3=new JLabel ("");
+		l4=new JLabel ("");
+		t1= new JTextField (10);		
+		t2= new JTextField (10);
+		add(b1);		
+		add(b2);
+		add(l1);
+		add(t1);
+		add(l2);
+		add(t2);
+		add(l3);
+		add(l4);
+	}
+	
 	}
 
-	public void Scan(int a, int b) {
-		
-		JOptionPane.showMessageDialog(null, "Теперь ваше первое число = " +a);
-		JOptionPane.showMessageDialog(null, "Теперь ваше второе число = " +b);			
-		
-	}
-	
-	
-	
-}
+
--- src/main/java/training1/start.java
+++ src/main/java/training1/start.java
@@ -1,26 +1,16 @@
 package training1;
 
-public class start {
+import javax.swing.JFrame;
 
-	public static void main(String[] args) {
-		Reader r= new Reader();
-		r.Scan();
-		r.i=count (r.i);
-		r.k=count (r.k);		
-		//System.out.println ("new 1 =" + r.i);
-		//System.out.println ("new 2 =" + r.k); 		
-		r.Scan (r.i,r.k);
-	
-	
-	
-	}
-	
-	//counter plus one
-	private static int count (int x){
-		x = x + 1;
-		return x;
+public class start {
+	public static void main (String args []){
+		Reader r = new Reader ("window name 666");
+		r.setVisible(true);  //set window visible
+		r.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//exit
+		r.setSize(300, 200);
+		r.setResizable(false);// don't resize window
+		r.setLocationRelativeTo(null);//in center 
+		
 		
 	}
-	
-	//end counter
 }
